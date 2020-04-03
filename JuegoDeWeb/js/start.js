82dc069963be3fb385d7746d50490a79430c9f83
GMarrades <45025223+GMarrades@users.noreply.github.com>
2020-04-03 13:29:01 +0200

    Add files via upload

---
+++ JuegoDeWeb/js/start.js
@@ -0,0 +1,46 @@
+/* ESTE ESPACIO ESTA PATROCINADO POR LAS VARIABLES GLOBALES Y LAS CONSTANTES*/
+
+
+
+
+let initialState = {
+    preload: loadAssets,
+    create: displayScreen
+};
+
+let btnPlayer, btnAbout, btnInstructions, btnLvl;
+
+function loadAssets(){
+
+}
+
+function displayScreen(){
+
+
+let textTitle = ""; //Falta titulo del videojuego este
+let styleTitle = {};
+
+game.add.text(50, game.world.height/6, textTitle, styleTitle);
+
+btnAbout = game.add.button(game.world.width / 1.75, game.world.height/3,
+    'aboutButton', onAboutButtonPressed);
+btnInstructions = game.add.button(game.world.width/1,75, game.world.height/3 +120,
+    'instructionsButon', onInstructionsButtonPressed);
+btnLvl = game.add.button(game.world.width / 1.75, game.world.height/3 +240,
+    'lvlButton', onLvlButtonPressed);
+btnPlayer = game.add.button(game.world.width /1.75, game.world.height/3 + 360,
+    'playerButton', onPlayerButtonPressed);
+}
+
+function onAboutButtonPressed(){
+    game.state.start("about");
+}
+function onLvlButtonPressed(){
+    game.state.start("lvlSelection");
+}
+function onInstructionsButtonPressed(){
+    game.state.start("instructions");
+}
+function onPlayerButtonPressed(){
+    game.state.start("player");
+}
