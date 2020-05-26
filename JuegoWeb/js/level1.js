ea6f4c188671ba22d78c31f950092b93e4dba23c
GMarrades <45025223+GMarrades@users.noreply.github.com>
2020-05-26 12:52:58 +0200

    Add files via upload

--- JuegoWeb/js/level1.js
+++ JuegoWeb/js/level1.js
@@ -126,11 +126,15 @@ function render(){
 
 //Leer del JSON para sacar los bloques
 let nivel = JSON.parse(Nivel1);
+let niv = [];
  for (let i=0; i<nivel.length; i++){
      let plataforma = JSON.parse(Nivel1[i+1]);
+     let plat = [];
      for(let j = 0; j<plataforma.length; j++){
         bloque = new Bloque(plataforma[j]);
+        plat.push(bloque);
      }
+     niv.push(plat);
  }
 
  function recibirDaño(daño){
--- JuegoWeb/js/level2.js
+++ JuegoWeb/js/level2.js
@@ -13,3 +13,16 @@ function displayScreen(){
     game.add.text(50, 50, " Level2",style);
 
 }
+
+//Leer del JSON para sacar los bloques
+let nivel = JSON.parse(Nivel1);
+let niv = [];
+ for (let i=0; i<nivel.length; i++){
+     let plataforma = JSON.parse(Nivel1[i+1]);
+     let plat = [];
+     for(let j = 0; j<plataforma.length; j++){
+        bloque = new Bloque(plataforma[j]);
+        plat.push(bloque);
+     }
+     niv.push(plat);
+ }
